938c1e7449ddb9723c7005f2c8a1703fd8214fb5
[DanielBayerri] <[bayerri4tgn@gmail.com]>
2022-03-11 14:30:51 +0100

    base de dades creada

---
+++ database/seeder.py
@@ -0,0 +1,31 @@
+import sqlite3 as sql
+
+DB_PATH = "C:\\Users\\DanielBayerri\\Desktop\\Salou\\Salou\\database\\salou.db"
+
+def create_database():
+    conn = sql.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute("""CREATE TABLE consulta (
+        numero integer,
+        doctor text,
+        llista text
+        """)
+    conn.commit()
+    conn.close()
+
+def add_database():
+    conn = sql.connect(DB_PATH)
+    cursor = conn.cursor()
+    data = [
+        (26, "Yolanda Ortega", "vendes")
+        (29, "Jordi Daniel", "vetadine, coliri")
+        (431, "Jesus", "vetadine, coliri, gasses")
+    ]
+    cursor.executemany("""INSERT INTO consulta VALUES (?, ?, ?)""", data)
+    conn.commit()
+    conn.close()
+    
+if __name__ == "__main__":
+    create_database()
+    add_database()
+
