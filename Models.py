25d8cb617ab425d84c5083af46532f02313209e0
[DanielBayerri] <[bayerri4tgn@gmail.com]>
2022-03-12 01:44:11 +0100

    primer dia, 30 minuts de vídeo

--- Models.py
+++ Models.py
@@ -4,8 +4,8 @@ db = SQLAlchemy()
 
 class Consultes(db.Model):
     rowid = db.Column(db.Integer, primary_key=True)
-    numero_consulta = db.Column(db.Integer, nullable = False)
-    nom_doctor = db.Column(db.String(100), unique = True, nullable = False)
-    llista_productes = db.Column(db.String(500), nullable = True)
+    numero = db.Column(db.Integer, nullable = False)
+    doctor = db.Column(db.String(100), unique = True, nullable = False)
+    llista = db.Column(db.String(500), nullable = True)
 
 
--- database/seeder.py
+++ database/seeder.py
@@ -5,11 +5,11 @@ DB_PATH = "C:\\Users\\DanielBayerri\\Desktop\\Salou\\Salou\\database\\salou.db"
 def create_database():
     conn = sql.connect(DB_PATH)
     cursor = conn.cursor()
-    cursor.execute("""CREATE TABLE consulta (
+    cursor.execute("""CREATE TABLE consultes (
         numero integer,
         doctor text,
         llista text
-        """)
+        )""")
     conn.commit()
     conn.close()
 
@@ -17,11 +17,11 @@ def add_database():
     conn = sql.connect(DB_PATH)
     cursor = conn.cursor()
     data = [
-        (26, "Yolanda Ortega", "vendes")
-        (29, "Jordi Daniel", "vetadine, coliri")
+        (26, "Yolanda Ortega", "vendes"),
+        (29, "Jordi Daniel", "vetadine, coliri"),
         (431, "Jesus", "vetadine, coliri, gasses")
     ]
-    cursor.executemany("""INSERT INTO consulta VALUES (?, ?, ?)""", data)
+    cursor.executemany("""INSERT INTO consultes VALUES (?, ?, ?)""", data)
     conn.commit()
     conn.close()
     
